2327f91749635d19e10108e8efe66cb9ce24c931
Josiah Hunsinger <jhunsinger2860@flpoly.org>
2016-08-24 20:34:45 +0000

    added require-package function

--- run.sh
+++ run.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 echo 'Starting proto-mesh Setup...'
 
+# Load config file
+source config
+
+# Enable the batman-adv kernal module
 modprobe batman-adv
+if [ $? != 0 ]; then echo 'batman-adv kernal module not present!';exit 1; fi
+
+# Verify that some packages are installed
+require-package (){
+   which $1 > /dev/null
+   if [ $? != 0 ]
+      then
+         echo "Package $1: Installing..."
+         apt-get install --assume-yes $1 > /dev/null
+         echo "Package $1: Complete."
+      else
+         echo "Package $1: Already installed."
+   fi
+}
+require-package batctl
+require-package dnsmasq
+require-package ip
+
 # May have to insert a delay here to give bat0 time to become available
 
 # Before we actually connect to the batman-adv mesh, lets see if we have internet of our own
 HAVE_INET=0
-ping -c 1 8.8.8.8
-if [ $? == 0 ]; then HAVE_INET=1;fi
+ping -c 1 8.8.8.8 > /dev/null
+if [ $? == 0 ] 
+   then 
+      HAVE_INET=1
+      echo 'Founded pre-existing internet connection!'
+   else
+      echo 'Unable to find pre-existing internet connection!'
+fi
 
 # Get our classic 802.11 ad-hoc network up
 ip link set wlan0 down
@@ -28,6 +56,8 @@ fi
 
 # See if we can get an IP configuration from a DHCP server already
 # on the network.
+# Set dhclient timeout time to something more reasonable
+sed -i s/'#timeout 60'/'timeout 5'/g /etc/dhcp/dhclient.conf 
 FIRST_ARRIVER=1
 dhclient -1 bat0 
 if [ $? == 0 ]
@@ -35,8 +65,9 @@ if [ $? == 0 ]
       echo 'Got an IP from the network!'
       FIRST_ARRIVER=0
    else
-      echo 'Could not get IP from the network.\nUsing default IP: $PERFERED_IP'
-      ip addr add $PERFERED_IP dev bat0
+      echo "Could not get IP from the network."
+      echo "Using default IP: $DEFAULT_EXIP"
+      ip addr add $DEFAULT_EXIP dev bat0
 fi 
 
 # If we have Internet access, we are starting a DHCP server no matter what 
@@ -48,8 +79,8 @@ if [ $HAVE_INET == 1 ]
    then 
       dnsmasq -i bat0 --dhcp-option=3,$DEFAULT_ADDR
    else
-      if [ $FIRST_ARRIVER == 1 ]:
-         dnsmasq -i bat0
+      if [ $FIRST_ARRIVER == 1 ]; then dnsmasq -i bat0; fi
 
+fi
 echo 'End of script'
 
