4186836738a194a59ae6f234be9798f715a5635a
Josiah Hunsinger <jhunsinger2860@flpoly.org>
2016-08-26 19:11:00 +0000

    make kadnode-start.sh insure peer acquisition before exiting

--- kadnode-start.sh
+++ kadnode-start.sh
@@ -8,3 +8,19 @@ if [ $? == 0 ]; then
 else
    echo "Kadnode experienced and error..."
 fi
+
+# Insure peers
+while true; do
+   PEERCOUNT=$(kadnode-ctl status | grep "DHT Nodes" | awk '{print $3}')
+   if [ $PEERCOUNT == '0' ]; then
+      echo "Searching for a Kadnode peer..."
+      while read ADDR; do
+         echo "Attempting to connect to $ADDR"
+         kadnode-ctl import $ADDR > /dev/null
+      done <node_addrs.txt
+      sleep 5
+   else
+      echo 'Kadenode peer found!'
+      break
+   fi
+done
