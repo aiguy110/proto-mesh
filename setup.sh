18ccd2ad6e047e4ca700b041b1cac5bba98d08de
Josiah Hunsinger <jhunsinger2860@flpoly.org>
2016-09-09 20:54:19 +0000

    setup.sh now working

--- setup.sh
+++ setup.sh
@@ -10,4 +10,9 @@ echo "# Start proto-mesh" > boot-snippet.sh
 echo "export MESH_DIR=$(pwd)" >> boot-snippet.sh
 echo 'bash $MESH_DIR/start.sh' >> boot-snippet.sh 
 
-
+# Efectively, insert this code into rc.local so it is run at start up
+file1=boot-snippet.sh
+file2=/etc/rc.local
+line=$(grep -n -e '^exit 0' $file2 | cut -d ":" -f 1)
+{ head -n $(($line-1)) $file2; cat $file1; tail -n +$line $file2; } > .temp
+mv .temp $file2
