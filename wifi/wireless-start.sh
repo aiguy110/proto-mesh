b4d3214f2c371337b532e130a610e4abe1e6a7f1
Josiah Hunsinger <jhunsinger2860@flploy.org>
2016-10-19 15:59:14 -0400

    created "config.sample" and removed "config" to force users to address their configurations imedeately. Also attempted to fix issue with some wifi cards.

--- wifi/wireless-start.sh
+++ wifi/wireless-start.sh
@@ -4,8 +4,8 @@
 source wifi/wireless-config
 
 # Get our classic 802.11 ad-hoc network up
-ip link set dev $WIRELESS_IFACE down
+ip link set down dev $WIRELESS_IFACE
 iwconfig $WIRELESS_IFACE mode ad-hoc
 iwconfig $WIRELESS_IFACE channel $CHANNEL
 iwconfig $WIRELESS_IFACE essid $ESSID
-ip link set dev $WIRELESS_IFACE up
+ip link set up dev $WIRELESS_IFACE
