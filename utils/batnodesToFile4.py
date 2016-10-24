202f2ce8ebffae1adcf565de149905191f0c7ab0
Zachary Weingarten <zdog1230@yahoo.com>
2016-10-24 18:38:14 -0400

    Systemd Integration

--- utils/batnodesToFile4.py
+++
@@ -1,8 +0,0 @@
-import sys
-import address_helper
-
-if __name__ == '__main__':
-   macs = address_helper.listBatmanMACs()
-   with open(sys.argv[1], 'w') as f:
-      for mac in macs:
-         f.write(address_helper.macToIPv4(mac)+'\n')
